c81bbe4b923d2f14ef330f3599dbc0c3eba57019
semihorhan <sorhan3.14@gmail.com>
2022-09-11 15:29:53 +0300

    finish main content

--- react-facts/components/Main.jsx
+++ react-facts/components/Main.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Main = () => {
     return (
-        <h1>Main Content</h1>
+        <main>
+            <h1 className='main--title'>Fun facts about React</h1>
+            <ul className='main--facts'>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has weel over 100K stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
 
